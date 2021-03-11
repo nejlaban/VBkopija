465208ddc3053ab5ab107e2314c6d4d12abbd0ba
nejlaban <nejla.bandic@stu.ibu.edu.ba>
2021-03-11 18:28:18 +0100

    header

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,12 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Header from '../components/header'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Hi peopleeee</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
