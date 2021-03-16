f2c5a43479e6484180904ca1ff3b093cad87945f
elmedinkarisik <edo.karisik1@gmail.com>
2021-03-16 18:50:39 +0100

    Create client_data.json and import gatsby-transformer-json dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,12 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
+      },
+    },
   ],
 }
