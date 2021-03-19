639e707db4eaa9181cb519449a0db98f6fb75901
elmedinkarisik <edo.karisik1@gmail.com>
2021-03-19 21:49:46 +0100

    Change read more blurred text to lorem ipsum

--- src/components/company_overview.js
+++ src/components/company_overview.js
@@ -14,36 +14,19 @@ const CompanyOverview = () => (
       value of your principals, intellectual property, business performance,
       markets and outlook.
     </p>
-    <div
-      className="container blurredTextDiv"
-    >
-      <p
-        className="blurredText widthAndHeight100"
-      >
+    <div className="container blurredTextDiv">
+      <p className="blurredText widthAndHeight100">
         Venture Plans supports high-tech "something" with an emphasis on growth,
-        marketing and go-to-market "something" blablabla blabla bla bla blabla bla
-        bla bla bla bla bla bla bla bla bla bla blabla blabla bla bla bla bla bla bla
-        blablablablabla blablablablabla blablablablabla blablablablabla
-        blablablablabla blablablablabla blablabla blablablablabla
-        blablablablabla blablablablabla blablablablabla blablablablabla
-        blablablablabla blablablablabla blablablablabla blablablablabla
-        blablablablabla blablablablabla blablablablabla blablablablabla
-        blablablablabla blablablablabla blablablablabla blablablablabla
-        blablablablabla blablablablabla blablablablabla blablablablabla
-        blablablablabla blablablablabla blablablablabla blablablablabla
-        blablablablabla blablablablabla blablablablabla blablablablabla
-        blablablablabla blablablablabla blablablablabla blablablablabla
-        blablablablabla blablablablabla blablablablabla blablablablabla
-        blablablablabla blablablablabla blablablablabla blablablablabla
-        blablablablabla blablablablabla
+        marketing and go-to-market "something" Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
-      <div
-        className="container readMoreButtonDiv"
-      >
-        <button className='readMoreButton'
-        >
-          read more ▾
-        </button>
+      <div className="container readMoreButtonDiv">
+        <button className="readMoreButton">read more ▾</button>
       </div>
     </div>
   </div>
